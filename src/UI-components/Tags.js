c2f67971dba60c8cae88ff7fddb19be4c61e098f
firo0707 <firo0707@gmail.com>
2022-09-30 16:17:38 +0800

    [update] tags

--- src/UI-components/Tags.js
+++ src/UI-components/Tags.js
@@ -1,26 +1,120 @@
 import { noop } from "lodash";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { themes } from "../theme-style";
-// import { randomMakeId } from "../tools";
+import { IoMdPricetag } from "react-icons/io";
+import { HiHashtag } from "react-icons/hi";
+import { CgTag } from "react-icons/cg";
 
-const StyledTags = styled.div``;
+const StyledTags = styled.div`
+  .svg-react-icons {
+    width: 18px;
+    height: 18px;
+    fill: ${({ props }) =>
+      props.status ? props.themeColor.main : props.themeColor.border};
+    color: ${({ props }) =>
+      props.status ? props.themeColor.main : props.themeColor.border};
+    transition: 0.2s;
+  }
+  .tag-value {
+    font-size: 14px;
+    color: ${({ props }) =>
+      props.status ? props.themeColor.main : props.themeColor.border};
+    transition: 0.2s;
+  }
+`;
+
+const StyledDefaultTag = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 6px 12px;
+  border-radius: 6px;
+  border: 1px solid
+    ${({ props }) =>
+      props.status ? props.themeColor.main : props.themeColor.border};
+  transition: 0.2s;
+  cursor: pointer;
+  .svg-react-icons {
+    margin-right: 6px;
+  }
+`;
+
+const StyledHashTag = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const StyledHorizontal = styled.div`
+  display: flex;
+  align-items: center;
+  .svg-react-icons {
+    margin-right: 6px;
+  }
+  cursor: pointer;
+`;
+
+const FatchTagType = ({
+  type = "defalut",
+  theme = "light",
+  status = false,
+  value = "",
+}) => {
+  switch (type) {
+    case "hashtag":
+      return (
+        <StyledHashTag>
+          <HiHashtag className="svg-react-icons" />
+          <div className="tag-value">{value}</div>
+        </StyledHashTag>
+      );
+    case "horizontal":
+      return (
+        <StyledHorizontal>
+          <CgTag className="svg-react-icons" />
+          <div className="tag-value">{value}</div>
+        </StyledHorizontal>
+      );
+    default:
+      return (
+        <StyledDefaultTag
+          props={{
+            themeColor: themes[theme] || themes,
+            status: status,
+          }}
+        >
+          <IoMdPricetag className="svg-react-icons" />
+          <div className="tag-value">{value}</div>
+        </StyledDefaultTag>
+      );
+  }
+};
 
 export const Tags = ({
   theme = "light",
-  type = "a",
+  type = "defalut",
   value = "tag",
   selected = false,
   onClick = noop,
 }) => {
-  const [selected, setSelected] = useState(selected);
+  const [selectedStatus, setSelectedStatus] = useState(selected);
   return (
     <StyledTags
       props={{
         themeColor: themes[theme] || themes,
+        status: selectedStatus,
+      }}
+      onClick={() => {
+        setSelectedStatus((prev) => !prev);
+        onClick(!selectedStatus);
       }}
     >
-      <div>{value}</div>
+      <FatchTagType
+        type={type}
+        theme={theme}
+        status={selectedStatus}
+        value={value}
+      />
     </StyledTags>
   );
 };
--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import { Input } from "../UI-components/Input";
 import { DatePicker } from "../UI-components/DatePicker";
 import { Tab } from "../UI-components/Tab";
 import { List } from "../UI-components/List";
+import { Tags } from "../UI-components/Tags";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -24,197 +25,259 @@ const GlobalStyle = createGlobalStyle`
 `;
 const StyledApp = styled.div`
   @keyframes blurIn {
-    0% { filter: blur(1rem); }
-    100% { filter: blur(0); }
+    0% {
+      filter: blur(1rem);
+    }
+    100% {
+      filter: blur(0);
+    }
   }
   .app-content {
-    box-sizing:border-box ;
-    padding:50px;
+    box-sizing: border-box;
+    padding: 50px;
   }
   .app-title {
     position: relative;
-    width:100% ;
-    background-color:#0f0b08 ;
+    width: 100%;
+    background-color: #0f0b08;
     height: 120px;
-    overflow:hidden ;
+    overflow: hidden;
   }
   .app-title .banner {
-    opacity:.8 ;
+    opacity: 0.8;
     position: absolute;
-    top:50% ;
-    left:50% ;
-    transform: translate(-50%,-50%);
-    width:500px;
-    height:100%;
-    background-image: ${({devEnv})=> `url(".${devEnv ? '/UI-Components' : ''}/imgs/banner.jpg")`};
-    background-position:center -50px;
-    background-size:500px ;
-    z-index:1 ;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 500px;
+    height: 100%;
+    background-image: ${({ devEnv }) =>
+      `url(".${devEnv ? "/UI-Components" : ""}/imgs/banner.jpg")`};
+    background-position: center -50px;
+    background-size: 500px;
+    z-index: 1;
     animation: blurIn 1s;
   }
   .app-title span {
     position: absolute;
-    bottom:10% ;
-    left:50% ;
-    transform: translate(-50%,0%);
-    font-size:14px ;
-    color:#fff ;
-    text-shadow:0 0 5px rgba(0,0,0,0.6) ;
-    z-index:2 ;
+    bottom: 10%;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    font-size: 14px;
+    color: #fff;
+    text-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
+    z-index: 2;
   }
   .app-caption {
-    font-size:16px;
-    font-weight:bold ;
-    margin-bottom:25px ;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 25px;
   }
   .theme-btns {
-    display:flex ;
-    align-items:center ;
-    margin-bottom:25px ;
+    display: flex;
+    align-items: center;
+    margin-bottom: 25px;
     button {
       border-radius: 50px;
-      padding:8px 16px;
-      border:0 ;
-      font-size:16px;
-      box-shadow: 0 0 5px rgba(0,0,0,0.3);
+      padding: 8px 16px;
+      border: 0;
+      font-size: 16px;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
       cursor: pointer;
     }
-    button:nth-child(1){
+    button:nth-child(1) {
       background-color: ${themes.light.bottom};
       color: ${themes.light.color};
       margin-right: 12px;
     }
-    button:nth-child(2){
+    button:nth-child(2) {
       background-color: ${themes.dark.bottom};
       color: ${themes.dark.color};
       margin-right: 12px;
     }
   }
   .demo-compnent {
-    display:flex ;
-    align-items:center ;
+    display: flex;
+    align-items: center;
     margin-bottom: 50px;
   }
   .compnent-value {
-    font-size:16px;
-    margin-left:20px;
+    font-size: 16px;
+    margin-left: 20px;
     span {
       color: ${({ theme }) => themes[theme].main};
     }
   }
+  .demo-seprate {
+    padding: 0 6px;
+  }
 `;
 export const App = () => {
-  const devEnv = process.env.NODE_ENV === 'development';
-  const [theme, setTheme] = useState('light')
-  const [dropInputDemoVal, setDropInputDemoVal] = useState('');
-  const [buttonDemoVal, setButtonDemoVal] = useState('');
+  const devEnv = process.env.NODE_ENV === "development";
+  const [theme, setTheme] = useState("light");
+  const [dropInputDemoVal, setDropInputDemoVal] = useState("");
+  const [buttonDemoVal, setButtonDemoVal] = useState("");
   const [switchDemoVal, setSwitchDemoVal] = useState(false);
-  const [inputDemoVal, setInputDemoVal] = useState('');
-  const [datePickerDemoVal, setDatePickerDemoVal] = useState('YYYY/MM/DD');
-  const [tabDemoVal, setTabDemoVal] = useState('Orange');
-  return <StyledApp theme={theme} devEnv={devEnv}>
-    <GlobalStyle theme={theme} />
-    <div className="app-title">
-      <span>ADAM UI</span>
-      <div className="banner"></div>
-    </div>
-    <div className="app-content">
-      <div className="theme-btns">
-        <button onClick={() => setTheme('light')}>light</button>
-        <button onClick={() => setTheme('dark')}>dark</button>
+  const [inputDemoVal, setInputDemoVal] = useState("");
+  const [datePickerDemoVal, setDatePickerDemoVal] = useState("YYYY/MM/DD");
+  const [tabDemoVal, setTabDemoVal] = useState("Orange");
+  return (
+    <StyledApp theme={theme} devEnv={devEnv}>
+      <GlobalStyle theme={theme} />
+      <div className="app-title">
+        <span>ADAM UI</span>
+        <div className="banner"></div>
       </div>
-      <div className="app-caption">[ 🍎 drop input ] demo :</div>
-      <div className="demo-compnent">
-        <DropInput
-          theme={theme}
-          datalist={[{ value: "Apple", id: 1 }, { value: "Orange", id: 2 }, { value: "Pear", id: 3 }, { value: "Lemon", id: 4 }, { value: "Apple1", id: 5 }, { value: "Orange2", id: 6 }, { value: "Pear3", id: 7 }, { value: "Lemon4", id: 8 }]}
-          onClick={(e) => setDropInputDemoVal(e)}
-        />
-        <div className="compnent-value">value: <span>{dropInputDemoVal.value}</span>, id: <span>{dropInputDemoVal.id}</span></div>
-      </div>
-      <div className="app-caption">[ 🍎 button ] demo :</div>
-      <div className="demo-compnent">
-        <Button
-          theme={theme}
-          onClick={() => setButtonDemoVal('clicked!')}
-        />
-        <div className="compnent-value">value: <span>{buttonDemoVal}</span></div>
-      </div>
-      <div className="app-caption">[ 🍎 switch ] demo :</div>
-      <div className="demo-compnent">
-        <Switch
-          theme={theme}
-          status={switchDemoVal}
-          onChange={() => setSwitchDemoVal(prev => !prev)}
-        />
-        <div className="compnent-value">value: <span>{switchDemoVal ? 'true' : 'false'}</span></div>
-      </div>
-      <div className="app-caption">[ 🍎 input ] demo :</div>
-      <div className="demo-compnent">
-        <Input
-          theme={theme}
-          datalist={["@fresco.tech", "@google.com"]}
-          onChange={(val) => setInputDemoVal(val)}
-        />
-        <div className="compnent-value">value: <span>{inputDemoVal}</span></div>
-      </div>
-      <div className="app-caption">[ 🍎 date picker ] demo :</div>
-      <div className="demo-compnent">
-        <DatePicker theme={theme}
-          onClick={(val) => setDatePickerDemoVal(val.selected.format('YYYY/MM/DD'))}
-        />
-        <div className="compnent-value">value: <span>{datePickerDemoVal}</span></div>
-      </div>
-      <div className="app-caption">[ 🍎 Tab ] demo :</div>
-      <div className="demo-compnent">
-        <Tab
-          theme={theme}
-          defaultId={2}
-          dataList={[
-            {
-              value: 'Apple', id: 1
-            }, {
-              value: 'Orange', id: 2
-            }, {
-              value: "Pear", id: 3
-            }, {
-              value: "Lemon", id: 4
+      <div className="app-content">
+        <div className="theme-btns">
+          <button onClick={() => setTheme("light")}>light</button>
+          <button onClick={() => setTheme("dark")}>dark</button>
+        </div>
+        <div className="app-caption">[ 🍎 drop input ] demo :</div>
+        <div className="demo-compnent">
+          <DropInput
+            theme={theme}
+            datalist={[
+              { value: "Apple", id: 1 },
+              { value: "Orange", id: 2 },
+              { value: "Pear", id: 3 },
+              { value: "Lemon", id: 4 },
+              { value: "Apple1", id: 5 },
+              { value: "Orange2", id: 6 },
+              { value: "Pear3", id: 7 },
+              { value: "Lemon4", id: 8 },
+            ]}
+            onClick={(e) => setDropInputDemoVal(e)}
+          />
+          <div className="compnent-value">
+            value: <span>{dropInputDemoVal.value}</span>, id:{" "}
+            <span>{dropInputDemoVal.id}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 button ] demo :</div>
+        <div className="demo-compnent">
+          <Button theme={theme} onClick={() => setButtonDemoVal("clicked!")} />
+          <div className="compnent-value">
+            value: <span>{buttonDemoVal}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 switch ] demo :</div>
+        <div className="demo-compnent">
+          <Switch
+            theme={theme}
+            status={switchDemoVal}
+            onChange={() => setSwitchDemoVal((prev) => !prev)}
+          />
+          <div className="compnent-value">
+            value: <span>{switchDemoVal ? "true" : "false"}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 input ] demo :</div>
+        <div className="demo-compnent">
+          <Input
+            theme={theme}
+            datalist={["@fresco.tech", "@google.com"]}
+            onChange={(val) => setInputDemoVal(val)}
+          />
+          <div className="compnent-value">
+            value: <span>{inputDemoVal}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 date picker ] demo :</div>
+        <div className="demo-compnent">
+          <DatePicker
+            theme={theme}
+            onClick={(val) =>
+              setDatePickerDemoVal(val.selected.format("YYYY/MM/DD"))
             }
-          ]}
-          onClick={(val) => setTabDemoVal(val.value)}
-        />
-        <div className="compnent-value">value: <span>{tabDemoVal}</span></div>
+          />
+          <div className="compnent-value">
+            value: <span>{datePickerDemoVal}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 Tab ] demo :</div>
+        <div className="demo-compnent">
+          <Tab
+            theme={theme}
+            defaultId={2}
+            dataList={[
+              {
+                value: "Apple",
+                id: 1,
+              },
+              {
+                value: "Orange",
+                id: 2,
+              },
+              {
+                value: "Pear",
+                id: 3,
+              },
+              {
+                value: "Lemon",
+                id: 4,
+              },
+            ]}
+            onClick={(val) => setTabDemoVal(val.value)}
+          />
+          <div className="compnent-value">
+            value: <span>{tabDemoVal}</span>
+          </div>
+        </div>
+        <div className="app-caption">[ 🍎 List ] demo :</div>
+        <div className="demo-compnent">
+          <List
+            theme={theme}
+            header="Single Data List"
+            data={[
+              "Racing car sprays burning fuel into crowd.",
+              "Japanese princess to wed commoner.",
+              "Australian walks 100km after outback crash.",
+            ]}
+          />
+        </div>
+        <div className="demo-compnent">
+          <List
+            theme={theme}
+            header="Multiple Data List"
+            dataType="multiple"
+            data={[
+              {
+                title: "data caption-1",
+                data: [
+                  "Racing car sprays burning fuel into crowd.",
+                  "Japanese princess to wed commoner.",
+                  "Australian walks 100km after outback crash.",
+                ],
+              },
+              {
+                title: "data caption-2",
+                data: [
+                  "Racing car sprays burning fuel into crowd.",
+                  "Japanese princess to wed commoner.",
+                  "Australian walks 100km after outback crash.",
+                ],
+              },
+              {
+                title: "data caption-3",
+                data: [
+                  "Racing car sprays burning fuel into crowd.",
+                  "Japanese princess to wed commoner.",
+                  "Australian walks 100km after outback crash.",
+                ],
+              },
+            ]}
+          />
+        </div>
+        <div className="app-caption">[ 🍎 Tags ] demo :</div>
+        <div className="demo-compnent">
+          <Tags />
+          <span className="demo-seprate">/</span>
+          <Tags type="hashtag" />
+          <span className="demo-seprate">/</span>
+          <Tags type="horizontal" />
+        </div>
       </div>
-      <div className="app-caption">[ 🍎 List ] demo :</div>
-      <div className="demo-compnent">
-        <List
-          theme={theme}
-          header='Single Data List'
-          data={['Racing car sprays burning fuel into crowd.', 'Japanese princess to wed commoner.', 'Australian walks 100km after outback crash.']}
-        />
-      </div>
-      <div className="demo-compnent">
-        <List
-          theme={theme}
-          header='Multiple Data List'
-          dataType='multiple'
-          data={[
-            {
-              title: 'data caption-1',
-              data: ['Racing car sprays burning fuel into crowd.', 'Japanese princess to wed commoner.', 'Australian walks 100km after outback crash.']
-            },
-            {
-              title: 'data caption-2',
-              data: ['Racing car sprays burning fuel into crowd.', 'Japanese princess to wed commoner.', 'Australian walks 100km after outback crash.']
-            },
-            {
-              title: 'data caption-3',
-              data: ['Racing car sprays burning fuel into crowd.', 'Japanese princess to wed commoner.', 'Australian walks 100km after outback crash.']
-            },
-          ]}
-        />
-      </div>
-      <div className="app-caption">[ 🍎 Tags ] demo :</div>
-    </div>
-  </StyledApp>
-}
+    </StyledApp>
+  );
+};
